649e878dde0c63eab70c0d4b1e7450658f38266f
Sebasgarci12 <garciaecheveriseb6@gmail.com>
2024-09-26 17:47:02 -0500

    Update cursos.php

--- pages/cursos.php
+++ pages/cursos.php
@@ -1,1 +1,158 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Plaster&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300..700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../sources/css/main.css">
+    <script src="../sources/tailwind.config.js"></script>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400..900&display=swap" rel="stylesheet">
+    <title>Document</title>
+  
+</head>
+<body>
+    <header>
+      <div class="icono-menu">
+          <img src="https://res.cloudinary.com/djv5yehct/image/upload/v1725568269/codezero/tdwf3gh1j0ptczfevnkj.png" id="icono-menu">
+      </div>  
+      <div class="cont-menu active" id="menu">
+        <ul>
+          <li><a href="./index.html">INICIO</a></li>
+            <li><a href="./acerca.html">ACERCA DE CODIGO DESDE 0</a></li>
+            <li><a href="./quienes.html">¿QUIENES SOMOS?</a></li>
+            <li><a href="./induccion.html">INDUCCIÓN</a></li>
+            <li><a href="./programas.html">PROGRAMAS Y LENGUAJES</a></li>
+            <li><a href="./cursos.html">CURSOS</a></li>
+        </ul>
+      </div>
+    <h1 class="tit1">CURSOS</h1>
+      <a href="./index2.html"><img src="../sources/img/v91_24.png" alt="Logo de la página" class="img1"></a>
+  </header>
+    <article class="articulo">
+        <div class="container">
+            <p class="tit2">Curso de Python</p>
+        </div>
+      <div class="container">
+        <img src="https://res.cloudinary.com/djv5yehct/image/upload/v1726773761/codezero/dnpapmobcutlpag6lwgz.png" alt="" class="img2">
+      </div>
+        <div class="container">
+          <p class="tit1"><a href="https://github.com/mouredev/Hello-Python">Link directo</p></a>
+        </div>
+        <div class="container">
+          <p class="cursos"><br> En este link podrás encontrar aspectos básicos de Python que te ayudarán a hacer programas sencillamente.</p>
+      </div>
+      <div class="container">
+        <p class="tit2">Curso de HTML5</p>
+    </div>
+  <div class="container">
+    <img src="https://res.cloudinary.com/djv5yehct/image/upload/v1726771635/codezero/uxouw31i9oevgby0kubd.png" alt="">
+  </div>
+    <div class="container">
+      <p class="tit1"><a href="https://drive.google.com/drive/folders/0By2pfNu9CViYdTNNTGRIRG10QW8">Link directo</p></a>
+    </div>
+    <div class="container">
+      <p class="cursos"><br>En este link podrás encontrar aspectos básicos de HTML5 que te ayudarán a maquetar tus páginas de una manera correctamente estructurada.</p>
+  </div>
+  <div class="container">
+    <p class="tit2">Curso de C# (C-Sharp)</p>
+</div>
+<div class="container">
+<img src="https://res.cloudinary.com/djv5yehct/image/upload/v1726771635/codezero/qucaeywirckt2euxdbyq.png" alt="">
+</div>
+<div class="container">
+  <p class="tit1"><a href="https://www.youtube.com/playlist?list=PLU8oAlHdN5BmpIQGDSHo5e1r4ZYWQ8m4B">Link directo</p></a>
+</div>
+<div class="container">
+  <p class="cursos"><br>En este link podrás ver una lista de reproducción en Youtube de 127 vídeos, donde paso a paso te explicarán como utilizar el lenguaje de programación C#</p>
+</div>
+<div class="container">
+  <p class="tit2">Cursos variados</p>
+</div>
+<div class="container">
+<img src="https://res.cloudinary.com/djv5yehct/image/upload/v1726771635/codezero/hscjdbhgjzmjdrj1re5m.png" alt="">
+</div>
+<div class="container">
+<p class="tit1"><a href="https://capacitateparaelempleo.org/">Link directo</p></a>
+</div>
+<div class="container">
+<p class="cursos"><br>En esta página no solo encontrarás cursos sobre programación (PHP, Javascript, Sitios web, C#, etc), también podrás encontrar cursos sobre agricultura, construccción, barbería y muchas cosas más, donde se incluye una certificación.</p>
+</div>
+    </article>
+    <footer>
+      <div class="lg:hidden mt-8 bg-slate-500 w-full p-4 text-center text-sm space-y-4 text-black">
+      <div class="flex justify-center">
+          <img src="https://res.cloudinary.com/dkvyzhgc6/image/upload/v1724792718/proyecto/iconos/o2z9zb2ocgh3kpsz1ijr.png" alt="Escudo 1" class="w-32 h-32">
+          <img src="https://res.cloudinary.com/djv5yehct/image/upload/v1724793091/codezero/eipimanvxagv0qwog30r.png" alt="Escudo 2" class="h-32 w-44">
+      </div>
+      <p class="fira-code-uniquifier px-4 text-xl">
+          Propósito:
+      </p>
+      <p class="fira-code-uniquifier text-left text-lg">
+        Esta pagina está creada como medio de ayuda para los estudiantes que aspiran entrar a la media técnica del colegio I.E Campo Valdés mediante un convenio con la Institucion Universitaria Pascual Bravo.   
+      </p>
+      <p class="px-4 text-left text-xl">
+          Dirección
+      </p>
+      <p class="fira-code-uniquifier text-left text-lg">
+          Calle 83 No 47 - 33, Barrio Campo Valdés, Medellín - Antioquia - Colombia
+      </p>
+      <p class="px-4 text-left text-xl">
+        Contáctanos 
+      </p>
+      <p class="fira-code-uniquifier text-left text-lg">
+        Correo: ie.campovaldes@medellin.gov.co
+      </p>
+      <p class="px-4 text-left text-xl">
+        Télefono: (604)211 82 34 - (604)233 68 97 
+      </p>
 
+  <!-- Footer -->
+      <p class="fira-code-uniquifier text-xl text-left">
+          © Copyright 2024 Todos los Derechos Reservados
+      </p>
+      <div class="flex justify-center space-x-4 mt-2">
+          <img href="#" class="text-teal-400"><img src="https://res.cloudinary.com/djv5yehct/image/upload/v1724793091/codezero/xmk7f9b8xgnlomfbiulp.png" alt="Facebook" class="w-32 h-32">
+          <img href="#" class="text-teal-400"><img src="https://res.cloudinary.com/djv5yehct/image/upload/v1724793091/codezero/lmwyvp9nvyqetqep7xcg.png" alt="Twitter" class=" w-20 h-20 mt-[24px]">
+      </div>
+</div>
+<div class="hidden lg:block mt-8 bg-slate-500 w-full p-4 text-center text-sm space-y-4 text-black">
+  <div class="flex">
+      <img src="https://res.cloudinary.com/dkvyzhgc6/image/upload/v1724792718/proyecto/iconos/o2z9zb2ocgh3kpsz1ijr.png" alt="Escudo 1" class="w-48 h-48">
+      <img src="https://res.cloudinary.com/djv5yehct/image/upload/v1724793091/codezero/eipimanvxagv0qwog30r.png" alt="Escudo 2" class="h-1/4 w-1/4 ms-[-95px] mt-[-20px]">
+      <p class="fira-code-uniquifier text-left text-base h-fit w-[24%] mt-[15px] ms-[-70px]">
+        Propósito: <br>
+        Esta pagina está creada como medio de ayuda para los estudiantes que aspiran entrar a la media técnica del colegio I.E Campo Valdés mediante un convenio con la Institucion Universitaria Pascual Bravo.
+      </p>
+      <div class="flex justify-center space-x-4 mt-2">
+          <img href="#" class="text-teal-400"><img src="https://res.cloudinary.com/djv5yehct/image/upload/v1724793091/codezero/xmk7f9b8xgnlomfbiulp.png" alt="Facebook" class="w-32 h-32 mt-[-10px]">
+          <img href="#" class="text-teal-400"><img src="https://res.cloudinary.com/djv5yehct/image/upload/v1724793091/codezero/lmwyvp9nvyqetqep7xcg.png" alt="Twitter" class=" w-20 h-20 mt-[113px]">
+      </div>
+      <h2 class="fira-code-uniquifier text-left text-ls h-fit w-[24%] mt-[15px] ms-[40px]">
+          Dirección<br>
+          Calle 83 No 47 - 33, Barrio Campo Valdés, Medellín - Antioquia - Colombia
+      </h2>
+      <h2 class="fira-code-uniquifier text-left text-ls h-fit w-[24%] mt-[100px] ms-[-318px]">
+        Contáctanos <br>
+          Télefono: (604)211 82 34 - (604)233 68 97 <br>
+          Correo: ie.campovaldes@medellin.gov.co
+      </h2>
+  </div>
+</div>
+</div>
+</footer>
+      <div class="derechos">
+        <p class="copy">© Copyright 2024 Todos los Derechos Reservados</p> 
+        <p class="tyc">Términos y condiciones</p>
+      </div>
+      <script src="../js/main.js"></script>
+</body>
+</html>
